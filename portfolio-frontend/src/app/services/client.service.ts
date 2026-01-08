98618a657ebf49944c34eed0a86593512262afd0
yashbhandva <yashbhandva01@gmail.com>
2026-01-08 20:43:34 +0530

    Update client.service.ts

--- portfolio-frontend/src/app/services/client.service.ts
+++ portfolio-frontend/src/app/services/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { ApiResponse } from '../models/api-response.model';
@@ -14,5 +14,15 @@ export class ClientService {
 
   getMyMessagesCount(email: string): Observable<ApiResponse<number>> {
     return this.http.get<ApiResponse<number>>(`${this.apiUrl}/client/messages/count?email=${email}`);
+  }
+
+  getMyProjectRequests(clientId: number): Observable<ApiResponse<any[]>> {
+    const headers = new HttpHeaders().set('userId', clientId.toString());
+    return this.http.get<ApiResponse<any[]>>(`${this.apiUrl}/client/project-requests`, { headers });
+  }
+
+  createProjectRequest(clientId: number, request: any): Observable<ApiResponse<any>> {
+    const headers = new HttpHeaders().set('userId', clientId.toString());
+    return this.http.post<ApiResponse<any>>(`${this.apiUrl}/client/project-requests`, request, { headers });
   }
 }
