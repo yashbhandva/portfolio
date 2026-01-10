ffcf75058bfa8e95dec1977d6db408e6471f9dcd
yashbhandva <yashbhandva01@gmail.com>
2026-01-10 19:41:43 +0530

    Update environment.prod.ts

--- portfolio-frontend/src/environments/environment.prod.ts
+++ portfolio-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-domain.com/api',
+  apiUrl: 'https://portfolio-latest-hqe4.onrender.com',
   appName: 'Portfolio Website'
 };
