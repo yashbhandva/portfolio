6a49afb9dc306e691cf36ec75d4393652dce301a
yashbhandva <yashbhandva01@gmail.com>
2026-01-10 13:16:03 +0530

    Create NotificationRepository.java

---
+++ portfolio/src/main/java/com/business/portfolio/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.business.portfolio.repository;
+
+import com.business.portfolio.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(Long recipientId);
+    long countByRecipientIdAndIsReadFalse(Long recipientId);
+}
