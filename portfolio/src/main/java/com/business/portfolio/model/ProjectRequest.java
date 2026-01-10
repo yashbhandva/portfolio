8749e191d5060babfe8882eae2dee3acc6159da6
yashbhandva <yashbhandva01@gmail.com>
2026-01-10 12:46:37 +0530

    Update ProjectRequest.java

--- portfolio/src/main/java/com/business/portfolio/model/ProjectRequest.java
+++ portfolio/src/main/java/com/business/portfolio/model/ProjectRequest.java
@@ -1,5 +1,6 @@
 package com.business.portfolio.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -15,6 +16,7 @@ public class ProjectRequest {
 
     @ManyToOne
     @JoinColumn(name = "client_id")
+    @JsonIgnore
     private User client;
 
     @ManyToOne
@@ -39,6 +41,7 @@ public class ProjectRequest {
 
     @ManyToOne
     @JoinColumn(name = "assigned_team_member_id")
+    @JsonIgnore
     private TeamMember assignedTeamMember;
 
     private LocalDateTime createdAt;
