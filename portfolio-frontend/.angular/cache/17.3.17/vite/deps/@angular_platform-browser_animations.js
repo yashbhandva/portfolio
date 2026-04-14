b29e68acded7e1c12b4e1587f2db4eab1c42640f
yashbhandva <yashbhandva01@gmail.com>
2026-04-14 23:00:54 +0530

    Update @angular_platform-browser_animations.js

--- portfolio-frontend/.angular/cache/17.3.17/vite/deps/@angular_platform-browser_animations.js
+++ portfolio-frontend/.angular/cache/17.3.17/vite/deps/@angular_platform-browser_animations.js
@@ -24,8 +24,8 @@ import {
   ɵɵdefineNgModule,
   ɵɵinject
 } from "./chunk-2DXLVW4Q.js";
-import "./chunk-4RMHXXWK.js";
 import "./chunk-LFVCTHGI.js";
+import "./chunk-4RMHXXWK.js";
 import "./chunk-AJN3JCM6.js";
 import {
   __objRest,
